567183ba4cdbdbaa58a4066471dcd8faa33fa495
Smith Zhang <smithzhang0427@gmail.com>
2023-01-10 17:09:30 -0500

    add webhook

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require('express')
 var app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser')
-
 // ENV Config
 require('dotenv').config()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // PayPal Config
 var paypal = require('paypal-rest-sdk')
@@ -25,11 +28,25 @@ app.use(bodyParser.json())
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 // use res.render to load up an ejs view file
 
 // index page
 app.get('/', function (req, res) {
   res.render('pages/index')
+})
+
+app.post('/webhook', function (req, res) {
+  console.log('WEBHOOK >>>>>>>>>>>>>>>>>', req.body)
+  if (req.body?.event_type === "PAYMENT.SALE.COMPLETED" && io) {
+    io.emit("webhooks", req.body)
+  }
 })
 
 app.post('/pay', function (req, res) {
@@ -77,167 +94,158 @@ app.post('/pay', function (req, res) {
       }
     }
   })
-
-  app.get('/success', (req, res) => {
-    const payerId = req.query.PayerID
-    const paymentId = req.query.paymentId
-
-    const execute_payment_json = {
-      payer_id: payerId,
-      transactions: [
-        {
-          amount: {
-            currency: `${data.cType}`,
-            total: `${data.amount}`,
-          },
-        },
-      ],
-    }
-
-    paypal.payment.execute(paymentId, execute_payment_json, function (
-      error,
-      payment,
-    ) {
-      if (error) {
-        console.log(error.response)
-        throw error
-      } else {
-        console.log(JSON.stringify(payment))
-        res.send('Success')
-      }
-    })
-  })
-
-  app.get('/cancel', (req, res) => res.send('Cancelled'))
 })
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID
+  const paymentId = req.query.paymentId
+
+  paypal.payment.get(paymentId, function (error, payment) {
+    if (error) {
+      console.log(error)
+      throw error
+    } else {
+      const execute_payment_json = {
+        payer_id: payerId,
+        transactions: [
+          {
+            amount: {
+              currency: `${payment.transactions[0].amount.currency}`,
+              total: `${payment.transactions[0].amount.total}`,
+            },
+          },
+        ],
+      }
+
+      paypal.payment.execute(paymentId, execute_payment_json, function (
+        error,
+        payment,
+      ) {
+        if (error) {
+          console.log(error.response)
+          throw error
+        } else {
+          res.render('pages/index')
+        }
+      })
+    }
+  })
+})
+
+app.get('/cancel', (req, res) => res.render('pages/index'))
 
 app.post('/pay-batch', async function (req, res) {
   const data = req.body
 
-  const clientId = process.env.CLIENT_ID
-  const clientSecret = process.env.CLIENT_SECRET
-  const environment = new paypal_.core.SandboxEnvironment(
-    clientId,
-    clientSecret,
-  )
-  const client = new paypal_.core.PayPalHttpClient(environment)
+  const sender_batch_id = Math.random().toString(36).substring(9)
 
-  const requestBody = {
+  const create_payout_json = {
     sender_batch_header: {
-      recipient_type: 'EMAIL',
-      email_message: 'SDK payouts test txn',
-      note: 'Enjoy your Payout!!',
-      sender_batch_id: '20230106',
-      email_subject: 'This is a test transaction from SDK',
+      sender_batch_id: sender_batch_id,
+      email_subject: 'You have a payment',
     },
     items: [
       {
-        note: 'Your Payout!',
+        recipient_type: 'EMAIL',
         amount: {
-          currency: `${data.cType}`,
           value: `${data.amount}`,
+          currency: `${data.cType}`,
         },
         receiver: `${data.email}`,
-        sender_item_id: '20230106_1',
+        note: 'Thank you.',
+        sender_item_id: `${sender_batch_id}_1`,
       },
       {
-        note: 'Your Payout!',
+        recipient_type: 'EMAIL',
         amount: {
-          currency: `${data.cType2}`,
           value: `${data.amount2}`,
+          currency: `${data.cType2}`,
         },
         receiver: `${data.email2}`,
-        sender_item_id: '20230106_2',
+        note: 'Thank you.',
+        sender_item_id: `${sender_batch_id}_2`,
       },
     ],
   }
 
-  // Construct a request object and set desired parameters
-  // Here, PayoutsPostRequest() creates a POST request to /v1/payments/payouts
-  let request = new paypal_.payouts.PayoutsPostRequest()
-  request.requestBody(requestBody)
-
-  // Call API with your client and get a response for your call
-  let createPayouts = async function () {
-    let response = await client.execute(request)
-    console.log(`Response: ${JSON.stringify(response)}`)
-    // If call returns body in response, you can get the deserialized version from the result attribute of the response.
-    console.log(`Payouts Create Response: ${JSON.stringify(response.result)}`)
-  }
-
-  let createResponse = await createPayouts()
-  if (createResponse.statusCode === 201) {
-    //Retrieve Payout Batch details
-    let payoutId = createResponse.result.batch_header.payout_batch_id
-    console.log('Retrieving Payout details for id - ' + payoutId)
-    let getResponse = await getPayout(payoutId, true)
-    if (getResponse.statusCode === 200) {
-      //Retrieve Payout Item details
-      let payoutItemId = getResponse.result.items[0].payout_item_id
-      console.log('Retrieving Payout Item details for id - ' + payoutItemId)
-      let getItemResponse = await getPayoutItem(payoutItemId, true)
-      if (getItemResponse.statusCode === 200) {
-        let i = 0
-        //Wail till Payout Batch status becomes SUCCESS to cancel an UNCLAIMED payout.
-        //This is just for demonstration, defer using this while integration
-        //Note: While integrating use Webhooks to get realtime Payouts Batch and Item status updates
-        do {
-          let checkPayoutComplete = await getPayout(payoutId)
-          await sleep(2000)
-          if (
-            checkPayoutComplete.result.batch_header.batch_status === 'SUCCESS'
-          ) {
-            //Cancel UNCLAIMED payout item
-            console.log(
-              'Cancelling unclaimed payout item for id - ' + payoutItemId,
-            )
-            let cancelResponse = await cancelPayoutItem(payoutItemId, true)
-            if (getItemResponse.statusCode === 200) {
+  paypal.payout.create(create_payout_json, async function (
+    error,
+    createResponse,
+  ) {
+    if (error) {
+      console.log(error.response)
+      throw error
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 10000))
+      let getResponse = await getPayout(
+        createResponse.batch_header.payout_batch_id,
+        true,
+      )
+      console.log('GET RESPONSE >>>>>', getResponse)
+      if (getResponse.httpStatusCode === 200) {
+        //Retrieve Payout Item details
+        let payoutItemId = getResponse.items[0].payout_item_id
+        console.log('Retrieving Payout Item details for id - ' + payoutItemId)
+        let getItemResponse = await getPayoutItem(payoutItemId, true)
+        console.log(getItemResponse)
+        if (getItemResponse.statusCode === 200) {
+          let i = 0
+          //Wail till Payout Batch status becomes SUCCESS to cancel an UNCLAIMED payout.
+          //This is just for demonstration, defer using this while integration
+          //Note: While integrating use Webhooks to get realtime Payouts Batch and Item status updates
+          do {
+            let checkPayoutComplete = await getPayout(payoutId, true)
+            await sleep(2000)
+            if (checkPayoutComplete.batch_header.batch_status === 'SUCCESS') {
+              //Cancel UNCLAIMED payout item
               console.log(
-                'Unclaimed payout item cancelled successfully for id - ' +
-                  payoutItemId,
-              )
-
-              //Run cancel failure scenario
-              console.log(
-                'Simulate failure on cancelling an already cancelled Payout item with id: ' +
-                  payoutItemId,
+                'Cancelling unclaimed payout item for id - ' + payoutItemId,
               )
               await cancelPayoutItem(payoutItemId, true)
-            } else {
-              console.error(
-                'Failed to cancel unclaimed payout item for id - ' +
+              if (getItemResponse.statusCode === 200) {
+                console.log(
+                  'Unclaimed payout item cancelled successfully for id - ' +
                   payoutItemId,
-              )
+                )
+
+                //Run cancel failure scenario
+                console.log(
+                  'Simulate failure on cancelling an already cancelled Payout item with id: ' +
+                  payoutItemId,
+                )
+                await cancelPayoutItem(payoutItemId, true)
+              } else {
+                console.error(
+                  'Failed to cancel unclaimed payout item for id - ' +
+                  payoutItemId,
+                )
+              }
+              break
             }
-            break
+            i++
+          } while (i < 5)
+          if (i === 5) {
+            console.error("Batch hasn't processed successfully yet!!")
           }
-          i++
-        } while (i < 5)
-        if (i === 5) {
-          console.error("Batch hasn't processed successfully yet!!")
+        } else {
+          console.error(
+            'Failed to retrieve payout item details for id - ' + payoutItemId,
+          )
         }
-      } else {
-        console.error(
-          'Failed to retrieve payout item details for id - ' + payoutItemId,
-        )
       }
-    } else {
-      console.error('Failed to retrieve payout details for id - ' + payoutId)
     }
-  } else {
-    console.error('Failed to create payout')
-  }
+  })
 })
 
 app.post('/pay-invoice', function (req, res) {
   var data = req.body
+  console.log(data, 'data')
 
   var create_invoice_json = {
     merchant_info: {
       email: `${data.invoice_email}`,
-      first_name: 'Dennis',
-      last_name: 'Doctor',
+      first_name: 'John',
+      last_name: 'Doe',
       business_name: `${data.invoice_cName}`,
       phone: {
         country_code: '001',
@@ -295,28 +303,73 @@ app.post('/pay-invoice', function (req, res) {
 
   paypal.invoice.create(create_invoice_json, function (error, invoice) {
     if (error) {
+      console.log(error, 'error')
       throw error
     } else {
       console.log('Create Invoice Response')
-      var invoiceId = invoice.id;
-
+      var invoiceId = invoice.id
+      console.log(invoiceId, 'id')
       paypal.invoice.send(invoiceId, function (error, rv) {
         if (error) {
-          console.log(error.response)
+          console.log(error.response, 'err')
           throw error
         } else {
           console.log('Send Invoice Response')
           console.log(rv)
+          res.render('pages/index')
         }
       })
     }
   })
 })
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function getPayout(payoutId, status) {
+  paypal.payout.get(payoutId, function (error, payout) {
+    if (error) {
+      console.log(error)
+      throw error
+    } else {
+      // console.log('Get Payout Response')
+      // console.log(JSON.stringify(payout))
+      return payout
+    }
+  })
+}
+
+async function getPayoutItem(payoutId, status) {
+  paypal.payoutItem.get(payoutId, function (error, payoutItem) {
+    if (error) {
+      console.log(error)
+      throw error
+    } else {
+      // console.log('Get payoutItem Response')
+      // console.log(JSON.stringify(payoutItem))
+      return payoutItem
+    }
+  })
+}
+
+async function cancelPayoutItem(payoutId, status) {
+  paypal.payoutItem.cancel(payoutId, function (error, payoutItemDetails) {
+    if (error) {
+      console.log(error.response)
+      throw error
+    } else {
+      // console.log('Cancel payoutItem Response')
+      // console.log(JSON.stringify(payoutItemDetails))
+      return payoutItemDetails
+    }
+  })
+}
+
 // Port Setup...
 const port = process.env.PORT || 8080
 
 // Start Server...
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on ${port}`)
 })
